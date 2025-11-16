a49fa0df16f159a42582ff80b3c72a707691ad07
ChemiaM <140431489+Chemia9@users.noreply.github.com>
2025-11-16 11:52:17 -0800

    commit all added files

---
+++ src/data_analysis.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+""" basic analysis script """
+
+def load_students(filename):
+    """ load the csv file as a list
+
+    Args:
+    filename: csv file of data
+
+    Returns: list of dictionaries with each element of list 
+    corresponding to a student
+    """
+
+    #TODO: read csv file by line
+    #TODO: create a dictionary for each line with variables as keys
+    #TODO: append each dictionary to a list
+
+def calculate_average_grade (students):
+    """ calculate average of all students grades
+
+    Args:
+    filename: the list of students data
+
+    Returns: float for average grade across students
+    """
+
+    #TODO: Make list of grades only
+    #TODO: calculate average
+
+def count_math_students (students):
+    """ count number of students in Math
+
+    Args:
+    students: the list of students data
+
+    Returns: integer number of students in math
+    """
+
+    #TODO: make list of subjects
+    #TODO: count number of maths in subjects
+
+def generate_report():
+
+def save_report(report, filename):
+
+def main():
+
---
+++ src/data_analysis_functions.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""Advanced analysis script"""
+
+def load_data(filename):
+    """ Generic loader that checks file extension
+
+    Args:
+    filename: any file type
+
+    Returns: string 'csv' or error if not csv
+    """
+
+    #TODO: check if file type is csv
+
+def load_csv (filename):
+
+def analyze_data (students):
+    """ return dictionary wit multiple statistics
+
+    Args:
+    students: list of student data
+
+    Returns: dictionary with average, min, max
+    """
+    #TODO: create a list of grades
+    #TODO: calculate average
+    #TODO: get min and max
+
+def analyze_grade_distribution (grades):
+    """ count grades by letter grade ranges
+
+    Args:
+    grades: list of grades
+
+    Returns: dictionary of counts and percentages
+    """
+
+    #TODO: define grade categories in dictionary
+    #TODO: count through grades
+    #TODO: add percentages to dictionary
+
+def save_results (results, filename):
+
+def main():
+
